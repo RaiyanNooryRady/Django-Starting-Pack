e0c1a413f1eb6117c4f016f29df24c604f7b64af
Raiyan Noory Rady <raiyannooryrady@gmail.com>
2023-08-22 17:42:07 +0600

    some modifications

--- core/settings.py
+++ core/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'home/templates/assets'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'home/templates/assets'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
